b5b76aed403bd1354e6aa78ecf377d1ebf74d974
K Mills <mustkre8@gmail.com>
2025-03-08 16:53:35 -0800

    Update Vitest configuration with path aliases

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vitest/config';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [svelte({ hot: !process.env.VITEST })],
+  resolve: {
+    alias: {
+      $lib: resolve('./src/lib'),
+      $app: resolve('./src/app_mock')
+    }
+  },
   test: {
     environment: 'jsdom',
     globals: true,
