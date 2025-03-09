bd87f2a26e2f55d7b057bb1bca5f7f45c280a7ff
K Mills <mustkre8@gmail.com>
2025-03-08 16:54:01 -0800

    Add app navigation mock for testing

---
+++ src/app_mock/navigation.js
@@ -0,0 +1,6 @@
+// Mock for $app/navigation
+export const goto = vi.fn();
+export const invalidate = vi.fn();
+export const prefetch = vi.fn();
+export const beforeNavigate = vi.fn();
+export const afterNavigate = vi.fn();
